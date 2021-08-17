b5aac289f145535a8c966473554a7603272d5aff
Ryan Smith <smith.a.ryan.98@gmail.com>
2021-08-16 21:56:51 -0400

    add(session): auto auth-renew

--- scalp-mechanic/session.py
+++ scalp-mechanic/session.py
@@ -9,8 +9,12 @@
 from __future__ import annotations
 import asyncio
 import json
-from asyncio import AbstractEventLoop, TimerHandle
-from datetime import datetime
+from asyncio import (
+    AbstractEventLoop, Task, TimerHandle
+)
+from datetime import (
+    datetime, timedelta, timezone
+)
 from typing import Optional
 
 import aiohttp
@@ -19,7 +23,9 @@ from aiohttp import (
     ClientWebSocketResponse
 )
 
-from utils import timestamp_to_datetime, urls
+from utils import (
+    timestamp_to_datetime, urls
+)
 from utils.errors import (
     LoginInvalidException, LoginCaptchaException, WebsocketException
 )
@@ -30,42 +36,53 @@ class Session:
     """Tradovate Session Class"""
 
     # -Constructor
-    def __init__(self, *, loop: Optional[AbstractEventLoop] = None) -> Session:
+    def __init__(
+        self, *, loop: Optional[AbstractEventLoop] = None,
+        authorization_renewal: bool = True
+    ) -> Session:
         self.authenticated: bool = False
-        self.token_expiration: Optional[datetime] = None
-        self.__session: Optional[ClientSession] = None
+        self._session: Optional[ClientSession] = None
+        self._socket: Optional[ClientWebSocketResponse] = None
+        self._token_expiration: Optional[datetime] = None
+        self._authorization_renewal: bool = authorization_renewal
+        self._authorization_handle: Optional[TimerHandle] = None
+        self._request_number: int = 0
         self._heartbeat_handle: Optional[TimerHandle] = None
-        self.__socket: Optional[ClientWebSocketResponse] = None
-        self.loop: AbstractEventLoop = loop if loop else asyncio.get_event_loop()
-        self.loop.run_until_complete(self.__async_init__())
+        self._loop: AbstractEventLoop = loop if loop else asyncio.get_event_loop()
+        self._loop.run_until_complete(self.__async_init__())
 
     # -Dunder Methods
     def __del__(self) -> None:
-        self.loop.run_until_complete(self.__async_del__())
+        self._loop.run_until_complete(self.__async_del__())
 
     async def __async_init__(self) -> None:
-        self.__session = aiohttp.ClientSession(loop=self.loop, raise_for_status=True)
-        self.__socket = await self.__session.ws_connect(urls.base_market_live)
-        if await self.__socket.receive_str() != 'o':
+        self._session = aiohttp.ClientSession(loop=self._loop, raise_for_status=True)
+        self._socket = await self._session.ws_connect(urls.base_market_live)
+        if await self._socket.receive_str() != 'o':
             raise WebsocketException()  # TODO: Better exception
-        self._request_number = 0
-        self._heartbeat_handle = self.loop.call_at(
-            self.loop.time() + 2.5, self._send_heartbeat
-        )
+        self._timer_heartbeat()
 
     async def __async_del__(self) -> None:
         # TODO: Close method with better control
-        if self.__session:
-            await self.__session.close()
-            await self.__socket.close()
+        if self._session:
+            await self._session.close()
+            await self._socket.close()
 
     # -Instance Methods: Private
+    def _receive_authorization(self, result: Task):
+        '''Recieving authorization renewal request through session'''
+        self._timer_authorization()
+
+    def _send_authorization(self) -> None:
+        '''Send authorization renewal request through session'''
+        print(f"Current token: {self._session.headers['AUTHORIZATION']}")
+        task = self._loop.create_task(self.renew_access_token())
+        task.add_done_callback(self._timer_authorization)
+
     def _send_heartbeat(self) -> None:
         '''Send heartbeat packet through websocket'''
-        self.loop.create_task(self.__socket.send_str("[]"))
-        self._heartbeat_handle = self.loop.call_at(
-            self.loop.time() + 2.5, self._send_heartbeat
-        )
+        self._loop.create_task(self._socket.send_str("[]"))
+        self._timer_heartbeat()
 
     async def _send_socket_request(
         self, url: str, query: str = "", body: str = ""
@@ -73,7 +90,20 @@ class Session:
         '''Send formatted request string through websocket'''
         req = f"{url}\n{self._request_number}\n{query}\n{body}"
         self._request_number += 1
-        await self.__socket.send_str(req)
+        await self._socket.send_str(req)
+
+    def _timer_authorization(self, task: Optional[Task] = None) -> None:
+        '''Timer handler for authorization renewal'''
+        self._authorization_handle = self._loop.call_later(
+            self.get_token_duration(timedelta(minutes=5)).total_seconds(),
+            self._send_authorization
+        )
+
+    def _timer_heartbeat(self) -> None:
+        '''Timer handler for heartbeat'''
+        self._heartbeat_handle = self._loop.call_later(
+            2.5, self._send_heartbeat
+        )
 
     async def _update_authorization(self, res: ClientResponse) -> dict[str, str]:
         '''Set authorization for active session'''
@@ -87,27 +117,43 @@ class Session:
                 bool(res_dict['p-captcha'])
             )
         # -Access Token
-        self.token_expiration = timestamp_to_datetime(res_dict['expirationTime'])
-        self.__session.headers.update({
+        self._token_expiration = timestamp_to_datetime(res_dict['expirationTime'])
+        self._session.headers.update({
             'AUTHORIZATION': "Bearer " + res_dict['accessToken']
         })
         return res_dict
 
+    # -Instance Methods: Temporary
+    def get_token_duration(self, offset: Optional[timedelta] = None) -> timedelta:
+        '''Get timedelta of remaining time until token is expired'''
+        time_remaining = self._token_expiration - datetime.now(timezone.utc)
+        if offset:
+            return time_remaining - offset
+        return time_remaining
+
+    def is_token_expired(self, offset: Optional[timedelta] = None) -> bool:
+        '''Returns true if current time in UTC is past the token expiration datetime'''
+        time = datetime.now(timezone.utc)
+        if offset:
+            return time >= self._token_expiration - offset
+        return time >= self._token_expiration
+
     # -Instance Methods
-    async def request_access_token(self, _dict: dict[str, str]) -> None:
+    async def request_access_token(self, dict_: dict[str, str]) -> None:
         '''Request session authorization'''
-        res = await self.__session.post(urls.auth_request, json=_dict)
+        res = await self._session.post(urls.auth_request, json=dict_)
         res_dict = await self._update_authorization(res)
         # -Market Token
         await self._send_socket_request("authorize", body=res_dict['mdAccessToken'])
-        #ws_res = json.loads((await self.__socket.receive()).data[1:])[0] -- one liner
-        ws_res = await self.__socket.receive()
+        ws_res = await self._socket.receive()
         ws_res_dict = json.loads(ws_res.data[1:])[0]
         if ws_res_dict['s'] != 200:
             raise WebsocketException()  # TODO: Better exception
         self.authenticated = True
+        if self._authorization_renewal:
+            self._timer_authorization()
 
     async def renew_access_token(self) -> None:
         '''Renew session authorization'''
-        res = await self.__session.post(urls.auth_renew)
+        res = await self._session.post(urls.auth_renew)
         await self._update_authorization(res)
