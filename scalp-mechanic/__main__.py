c8c7bc7f620a7ba4053c2c41dc583e601e4db0e4
Ryan Smith <smith.a.ryan.98@gmail.com>
2021-08-26 02:15:03 -0400

    add: logging

--- scalp-mechanic/__main__.py
+++ scalp-mechanic/__main__.py
@@ -13,25 +13,69 @@ $200/day + commissions
 """
 
 ## Imports
+import logging
 from configparser import ConfigParser
+from datetime import date
 
 from client import Client
 
 ## Constants
-account = ConfigParser()
-scalp_mechanic = Client()
+logging.basicConfig(
+    filename=f"logs/{date.today()}.log",
+    encoding='utf-8',
+    format="[%(asctime)s:%(msecs)d]%(levelname)s: %(message)s",
+    datefmt="%Y/%m/%d %H:%M:%S",
+    level=logging.DEBUG
+)
+client_log = logging.getLogger("Scalp-Mechanic")
+credentials = ConfigParser()
 
-## Functions
+
+## Classes
+class Scalp_Mechanic():
+    """Scalp-Mechanic Tradovate Client"""
+
+    async def on_connect(self) -> None:
+        print("On Connect")
+
+    async def on_ready(self) -> None:
+        print("On Ready")
+
+    async def on_alert(self, alert) -> None:
+        print("On Alert")
+        print(f"Alert Data: {alert}")
+
+    async def on_market_update(self, market) -> None:
+        print("On Market Update")
+        print(f"Market Data: {market}")
+
+    async def on_dom_update(self, dom) -> None:
+        print("On DOM Update")
+        print(f"DOM Data: {dom}")
+
+    async def on_chart_update(self, chart) -> None:
+        print("On Chart Update")
+        print(f"Chart Data: {chart}")
+
+    async def on_histogram_update(self, histogram) -> None:
+        print("On Histogram Update")
+        print(f"Histogram Data: {histogram}")
+
+    async def on_order_update(self, order) -> None:
+        print("On Order Update")
+        print(f"Order Data: {order}")
+
 
 ## Body
-account.read("account.ini")
-authorization = {
-    'name': account['authentication']['username'],
-    'password': account['authentication']['password'],
-    'deviceId': account['application']['device_id'],
-    'cid': account['authentication']['cid'],
-    'sec': account['authentication']['security_key'],
-    'appID': account['application']['name'],
-    'appVersion': account['application']['version'],
+scalp_mechanic = Scalp_Mechanic()
+credentials.read("account.ini")
+authorization_dict = {
+    'name': credentials['authentication']['username'],
+    'password': credentials['authentication']['password'],
+    'deviceId': credentials['application']['device_id'],
+    'cid': credentials['authentication']['cid'],
+    'sec': credentials['authentication']['security_key'],
+    'appID': credentials['application']['name'],
+    'appVersion': credentials['application']['version'],
 }
-scalp_mechanic.run(authorization)
+scalp_mechanic.run(authorization_dict)
