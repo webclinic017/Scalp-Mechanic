63737788a129b396b8f45b77009259b545e86da5
Ryan Smith <smith.a.ryan.98@gmail.com>
2021-09-01 17:15:01 -0400

    refactor: session, websocket, client

---
+++ scalp-mechanic/client.py
@@ -0,0 +1,77 @@
+##-------------------------------##
+## [Tradovate] Scalp-Mechanic    ##
+## Written By: Ryan Smith        ##
+##-------------------------------##
+## Tradovate Client              ##
+##-------------------------------##
+
+## Imports
+from __future__ import annotations
+import asyncio
+import logging
+from asyncio import (
+    AbstractEventLoop, Task, TimerHandle
+)
+from typing import Optional
+
+from profile import Profile
+from utils import urls
+from utils.session import Session, TradovateWebSocket
+
+## Constants
+log = logging.getLogger(__name__)
+
+
+## Classes
+class Client(Profile):
+    """Tradovate Client Class"""
+
+    # -Constructor
+    def __init__(self) -> Client:
+        self._loop: AbstractEventLoop = asyncio.new_event_loop()
+        super().__init__(Session(loop=self._loop))
+        self._authorization_handle: Optional[TimerHandle] = None
+
+    # -Instance Methods: Private
+    def _renew_authorization(self) -> None:
+        '''Send authorization renewal request'''
+        log.debug("Auto-renewing client authorization")
+        task = self._loop.create_task(self._session.renew_access_token())
+        task.add_done_callback(self._timer_authorization)
+
+    def _timer_authorization(self, result: Optional[Task] = None) -> None:
+        '''Timer handler for authorization renewal'''
+        if result:
+            result.result()
+        self._authorization_handle = self._loop.call_later(
+            self._session.get_token_duration().total_seconds() - 1000
+        )
+
+    # -Instance Methods: Public
+    async def authorize(self, dict_: dict[str, str], renew_authorization: bool) -> None:
+        '''Initialize and setup auto-renewal for client authorization'''
+        self._socket = await TradovateWebSocket.from_client(self, urls.base_market_live)
+        self.id = await self._session.request_access_token(dict_, self._socket)
+        if renew_authorization:
+            self._timer_authorization()
+
+    async def close(self) -> None:
+        await self._socket.close()
+        await self._session.close()
+
+    def get_loop(self) -> Session:
+        '''Returns async event loop'''
+        return self._loop
+
+    def get_session(self) -> Session:
+        '''Returns client active session'''
+        return self._session
+
+    def run(self, dict_: dict[str, str], *, renew_authorization: bool = True) -> None:
+        '''Run client main loop'''
+        self._loop.run_until_complete(self.authorize(dict_, renew_authorization))
+        try:
+            self._loop.run_forever()
+        except KeyboardInterrupt:
+            self._loop.run_until_complete(self.close())
+            self._loop.stop()
---
+++ scalp-mechanic/utils/session.py
@@ -0,0 +1,210 @@
+##-------------------------------##
+## [Tradovate] Scalp-Mechanic    ##
+## Written By: Ryan Smith        ##
+##-------------------------------##
+## Tradovate Session Details     ##
+##-------------------------------##
+
+## Imports
+from __future__ import annotations
+import asyncio
+import json
+import logging
+from typing import Optional, TYPE_CHECKING
+from asyncio import (
+    AbstractEventLoop, TimerHandle
+)
+from datetime import (
+    datetime, timedelta, timezone
+)
+
+from aiohttp import (
+    ClientSession, ClientResponse,
+    ClientWebSocketResponse as ClientWebSocket
+)
+
+from utils import (
+    timestamp_to_datetime, urls
+)
+from utils.errors import (
+    LoginInvalidException, LoginCaptchaException
+)
+if TYPE_CHECKING:
+    from client import Client
+
+## Constants
+log = logging.getLogger(__name__)
+
+
+## Classes
+class TradovateWebSocket:
+    """Tradovate WebSocket"""
+
+    # -Constructor
+    def __init__(
+        self, socket: ClientWebSocket,
+        *, loop: Optional[AbstractEventLoop] = None
+    ) -> TradovateWebSocket:
+        self.connected: bool = False
+        self.authenticated: bool = False
+        self._request: int = 0
+        self._heartbeat_handle: Optional[TimerHandle] = None
+        self._loop: AbstractEventLoop = loop if loop else asyncio.get_event_loop()
+        self._socket: Optional[ClientWebSocket] = socket
+        self._loop.create_task(self.__async_init__())
+
+    # -Dunder Methods
+    async def __async_init__(self) -> None:
+        if await self._socket.receive_str() != 'o':
+            raise Exception()  # TODO: Better exception
+        self.connected = True
+        self._timer_heartbeat()
+
+    # -Instance Methods: Private
+    def _send_heartbeat(self) -> None:
+        '''Send heartbeat packet to websocket'''
+        log.debug("Sending heartbeat")
+        self._loop.create_task(self._socket.send_str("[]"))
+        self._timer_heartbeat()
+
+    async def _socket_recieve(self) -> dict[str, str]:
+        '''Recieve dictionary object from websocket'''
+        ws_res = await self._socket.receive()
+        ws_res_dict = json.loads(ws_res.data[1:])
+        return ws_res_dict
+
+    async def _socket_send(
+        self, url: str, query: str = "", body: str = ""
+    ) -> None:
+        '''Send formatted request string to websocket'''
+        req = f"{url}\n{self._request}\n{query}\n{body}"
+        self._request += 1
+        await self._socket.send_str(req)
+
+    def _timer_heartbeat(self) -> None:
+        '''Timer handle for websocket heartbeat'''
+        self._heartbeat_handle = self._loop.call_later(
+            2.5, self._send_heartbeat
+        )
+
+    # -Instance Methods: Public
+    async def authorize(self, token: str) -> None:
+        '''Authorize websocket connection'''
+        await self._socket_send(urls.auth_websocket, body=token)
+        ws_res = (await self._socket_recieve())[0]
+        if ws_res['s'] != 200:
+            raise Exception()  # TODO: Better exception
+        self.authenticated = True
+
+    async def close(self) -> None:
+        if self._socket:
+            await self._socket.close()
+
+    # -Class Methods
+    @classmethod
+    async def from_client(
+        cls, client: Client, url: str, *, loop: Optional[AbstractEventLoop] = None
+    ) -> TradovateWebSocket:
+        '''Create a WebSocket from a Client object'''
+        loop = loop if loop else client.get_loop()
+        ws = await client.get_session().create_websocket(url)
+        return cls(ws, loop=loop)
+
+    @classmethod
+    async def from_session(
+        cls, session: Session, url: str, *, loop: Optional[AbstractEventLoop] = None
+    ) -> TradovateWebSocket:
+        '''Create a WebSocket from a Session object'''
+        # For Tradovate Library Purposes
+        loop = loop if loop else session.get_loop()
+        ws = await session.create_websocket(url)
+        return cls(ws, loop=loop)
+
+
+class Session:
+    """Tradovate HTTP Session"""
+
+    # -Constructor
+    def __init__(self, *, loop: Optional[AbstractEventLoop] = None) -> Session:
+        self.authenticated: bool = False
+        self._loop: AbstractEventLoop = loop if loop else asyncio.get_event_loop()
+        self._session: Optional[ClientSession] = None
+        self._loop.create_task(self.__async_init__())
+
+    # -Dunder Methods
+    async def __async_init__(self) -> None:
+        self._session = ClientSession(loop=self._loop, raise_for_status=True)
+
+    # -Instance Methods: Private
+    async def _update_authorization(self, res: ClientResponse) -> dict[str, str]:
+        '''Set authorization for active session'''
+        res_dict = await res.json()
+        # -Invalid Credentials
+        if 'errorText' in res_dict:
+            log.error("Login failure - " + res_dict['errorText'])
+            self.authenticated = False
+            raise LoginInvalidException(res_dict['errorText'])
+        # -Invalid Captcha
+        elif 'p-ticket' in res_dict:
+            ticket = res_dict['p-ticket']
+            retry = int(res_dict['p-time'])
+            captcha = bool(res_dict['p-captcha'])
+            self.authenticated = False
+            raise LoginCaptchaException(ticket, retry, captcha)
+        # -Access Token
+        self.authenticated = True
+        self._token_expiration = timestamp_to_datetime(res_dict['expirationTime'])
+        self._session.headers.update({
+            'AUTHORIZATION': "Bearer " + res_dict['accessToken']
+        })
+        return res_dict
+
+    # -Instance Methods: Public
+    async def close(self) -> None:
+        if self._session:
+            await self._session.close()
+
+    async def get(self, url: str, *args, **kwargs) -> dict[str, str]:
+        res = await self._session.request('GET', url, *args, **kwargs)
+        return await res.json()
+
+    def get_loop(self) -> AbstractEventLoop:
+        '''Returns async event loop'''
+        return self._loop
+
+    def get_token_duration(self, offset: Optional[timedelta] = None) -> timedelta:
+        '''Get timedelta of remaining time until token is expired'''
+        time_remaining = self._token_expiration - datetime.now(timezone.utc)
+        if offset:
+            return time_remaining - offset
+        return time_remaining
+
+    def is_token_expired(self, offset: Optional[timedelta] = None) -> bool:
+        '''Returns true if current time is past the token expiration datetime'''
+        time = datetime.now(timezone.utc)
+        if offset:
+            return time >= self._token_expiration - offset
+        return time >= self._token_expiration
+
+    async def post(self, url: str, *args, **kwargs) -> dict[str, str]:
+        res = await self._session.request('POST', url, *args, **kwargs)
+        return await res.json()
+
+    async def renew_access_token(self) -> None:
+        '''Renew session authorization'''
+        res = await self._session.post(urls.auth_renew)
+        await self._update_authorization(res)
+
+    async def request_access_token(
+        self, dict_: dict[str, str], websocket: Optional[TradovateWebSocket] = None
+    ) -> int:
+        '''Request session authorization'''
+        res = await self._session.post(urls.auth_request, json=dict_)
+        res_dict = await self._update_authorization(res)
+        if websocket:
+            await websocket.authorize(res_dict['mdAccessToken'])
+        return res_dict['userId']
+
+    async def create_websocket(self, url: str, *args, **kwargs) -> ClientWebSocket:
+        '''Create an aiohttp websocket response'''
+        return await self._session.ws_connect(url, *args, **kwargs)
