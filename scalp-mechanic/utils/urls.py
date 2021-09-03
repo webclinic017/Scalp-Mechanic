80506b4e3cfdf7f3deabbeafe5dca82ab8698629
Ryan Smith <smith.a.ryan.98@gmail.com>
2021-09-03 04:45:48 -0400

    add(urls): http and wss endpoints

--- scalp-mechanic/utils/urls.py
+++ scalp-mechanic/utils/urls.py
@@ -6,15 +6,26 @@
 ##-----------------------------##
 
 ## Constants
-# -Base URLS
-base_live = "https://live.tradovateapi.com/v1/"
-base_demo = "https://demo.tradovateapi.com/v1/"
-base_market_live = "wss://md.tradovateapi.com/v1/websocket"
-base_market_demo = "wss://md-demo.tradovateapi.com/v1/websocket"
+# -Base URLs
+_domains = (
+    "://live.tradovateapi.com/v1/",     # -Live Account Functionality
+    "://demo.tradovateapi.com/v1/",     # -Demo Account Functionality
+    "://md.tradovateapi.com/v1/",       # -Live Market Functionality
+    #"://md-demo.tradovateapi.com/v1/",  # -Demo Market Functionality
+    #"://replay.tradovateapi.com/v1",    # -Replay Market Functionality
+)
+http_base_demo, http_base_live, http_base_market, http_base_market_demo = [
+    f"https{domain}" for domain in _domains
+]
+wss_base_demo, wss_base_live, wss_base_market, wss_base_market_demo = [
+    f"wss{domain}websocket" for domain in _domains
+]
 # -Authorization
-base_auth = base_live + "auth/"
-auth_oauth = base_auth + "oauthtoken"
-auth_request = base_auth + "accesstokenrequest"
-auth_renew = base_auth + "renewaccesstoken"
-auth_me = base_auth + "me"
-auth_websocket = "authorize"
+# -HTTP[Live Only]
+http_base_auth = http_base_live + "auth/"
+http_auth_oauth = http_base_auth + "oauthtoken"
+http_auth_request = http_base_auth + "accesstokenrequest"
+http_auth_renew = http_base_auth + "renewaccesstoken"
+http_auth_me = http_base_auth + "me"
+# -Websocket
+wss_base_auth = "authorize"
