e3b3e071fe905d4219dac77b5eb2b15e8ae746b5
Ryan Smith <smith.a.ryan.98@gmail.com>
2021-09-04 17:27:41 -0400

    refactor: url domains

--- scalp-mechanic/utils/urls.py
+++ scalp-mechanic/utils/urls.py
@@ -11,13 +11,13 @@ _domains = (
     "://live.tradovateapi.com/v1/",     # -Live Account Functionality
     "://demo.tradovateapi.com/v1/",     # -Demo Account Functionality
     "://md.tradovateapi.com/v1/",       # -Live Market Functionality
-    #"://md-demo.tradovateapi.com/v1/",  # -Demo Market Functionality
+    #"://md-demo.tradovateapi.com/v1/",  # -Demo Market Functionality [?]
     #"://replay.tradovateapi.com/v1",    # -Replay Market Functionality
 )
-http_base_demo, http_base_live, http_base_market = [
+http_base_live, http_base_demo, http_base_market = [
     f"https{domain}" for domain in _domains
 ]
-wss_base_demo, wss_base_live, wss_base_market = [
+wss_base_live, wss_base_demo, wss_base_market = [
     f"wss{domain}websocket" for domain in _domains
 ]
 # -Authorization
@@ -28,4 +28,4 @@ http_auth_request = http_base_auth + "accesstokenrequest"
 http_auth_renew = http_base_auth + "renewaccesstoken"
 http_auth_me = http_base_auth + "me"
 # -Websocket
-wss_base_auth = "authorize"
+wss_auth = "authorize"
